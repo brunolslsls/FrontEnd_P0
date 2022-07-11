0fb8309ac78d4ac2cb2e8648a6e1ae09b02084ad
BrunoLS <brunolopessoares80@gmail.com>
2022-07-11 15:42:22 -0300

    Modal inicial

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, TextInput, TouchableOpacity, SafeAreaView, Flat
 import { Ionicons } from '@expo/vector-icons';
 import UsuarioList from './src/components/UsuarioList'; // importando uma classe
 import * as Animatable from 'react-native-animatable';
+import { Modal } from 'react-native';
 
 const BotaoAnimado = Animatable.createAnimatableComponent(TouchableOpacity)
 
@@ -17,9 +18,9 @@ export default function App() {
 
   ]);
 
-  function calcular() {
-    alert("s")
-  }
+  const [abrir, setAbrir] = useState(false);
+
+
   return (
     {/* style={estilos.container} isso como fosse style='background-color: red' */ },
     {/* linha de baixo vai jogar o estilo do 'container' para 'View'  */ },
@@ -30,6 +31,7 @@ export default function App() {
       </View>
 
 
+
       <FlatList /*FlatList é um elemento que pode criar lista de alguma coisa */
         marginHorizontal={20} // adiciona uma margem na horizontal para cada item
         showsHorizontalScrollIndicator={false} // barra de rolagem não aparece
@@ -38,15 +40,37 @@ export default function App() {
         renderItem={({ item }) => < UsuarioList data={item} />} // Data recebe o objetos criandos
       ></FlatList>
 
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={abrir}
+      >
+        <SafeAreaView style={estilos.modal}>
+          <View style={estilos.modalHeader}>
+            <TouchableOpacity
+              onPress={() => setAbrir(false)}
+            >
+              <Ionicons style={{ marginLeft: 5, marginRight: 5 }} name="md-arrow-back" size={35} color="#FFF"></Ionicons>
+            </TouchableOpacity>
+            <Text style={estilos.textoModal}>Janela Modal Teste</Text>
+          </View>
+
+
+        </SafeAreaView>
+
+      </Modal>
+
+
+
       <BotaoAnimado
         animation="bounceInUp"   // tipo de animação
         useNativeDriver        // Drive que precisa utilizar com  Animatable
         duration={1000} //A duração da animação
 
         style={estilos.botao} // Defina um estilo para o botão
-        onPress={calcular}  // chama uma função apois de precionar esse botão
+        onPress={() => setAbrir(true)}  // chama uma função apois de precionar esse botão
       >
-        <Ionicons ns name='ios-add' size={35} color='white' // cria um icone com um tamanho e cor 
+        <Ionicons name='ios-add' size={35} color='white' // cria um icone com um tamanho e cor 
         />
       </BotaoAnimado>
 
@@ -81,7 +105,7 @@ const estilos = StyleSheet.create({
     height: 50, // altura do elemento
     right: 25,  // posicao dele na direita
     bottom: 25, // posicao dele a baixo
-    zIndex: 999, // nivel da camada, quanto maior for numero mais na frente vai estar
+    zIndex: 9, // nivel da camada, quanto maior for numero mais na frente vai estar
     elevation: 5,  // intencidade da sombra
     shadowColor: 'red', // cor da sombra
     shadowOpacity: 0.2, // Nivel de opacidade da sombra
@@ -93,5 +117,26 @@ const estilos = StyleSheet.create({
 
 
   },
+
+  modal: {
+    flex: 1,      // Tamanho do item 
+    backgroundColor: '#111',
+
+  },
+  textoModal: {
+    color: '#767474',
+    marginTop: 5,
+    marginLeft: 10,
+    fontSize: 20,
+
+  },
+
+  modalHeader: {
+
+    marginLeft: 10,
+    marginTop: 20,
+    flexDirection: 'row',
+  },
+
 
 });
